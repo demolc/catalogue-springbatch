d8bba869b9cb0c6a2ba74fe934687520ee906fcd
ludivineUp <44409644+ludivineUp@users.noreply.github.com>
2025-03-06 12:14:28 +0100

    param

--- src/main/java/com/springbatch/catalogue/tasklets/ParameterizedTasklet.java
+++ src/main/java/com/springbatch/catalogue/tasklets/ParameterizedTasklet.java
@@ -1,15 +1,15 @@
 package com.springbatch.catalogue.tasklets;
 
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("step")
+@StepScope
 public class ParameterizedTasklet implements Tasklet {
 
     @Value("#{jobParameters['message']}")
